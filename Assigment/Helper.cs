c33a6a2af048f04c85a590c14181177f7626594a
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-05 19:39:33 +0200

    Q1: Bubble Sort

---
+++ Assigment/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment
+{
+    internal static class Helper
+    {
+        public static void BubbleSort<T>(T[] Arr) where T : IComparable<T>
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                for (int j = 0; j < Arr.Length - 1 - i; j++)
+                {
+                    if (Arr[j].CompareTo(Arr[j + 1]) == 1)
+                    {
+                        T Temp = Arr[j];
+                        Arr[j] = Arr[j + 1];
+                        Arr[j + 1] = Temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assigment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1: Bubble Sort 
+
+            int[] Num = { 1, 2, 5, 3, 4 };
+            Helper.BubbleSort(Num);
+
+            foreach (var item in Num) 
+            {
+                Console.Write($"{item}\t");
+            
+            }
+            #endregion
+        }
+    }
+}
