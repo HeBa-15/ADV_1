b13994e14840930d3267ef00a3b9c7599bd2f7be
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-05 20:14:50 +0200

    Q2: Create a generic Range<T> class

--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -6,15 +6,32 @@
         {
             #region Q1: Bubble Sort 
 
-            int[] Num = { 1, 2, 5, 3, 4 };
-            Helper.BubbleSort(Num);
+            //int[] Num = { 1, 2, 5, 3, 4 };
+            //Helper.BubbleSort(Num);
 
-            foreach (var item in Num) 
-            {
-                Console.Write($"{item}\t");
-            
-            }
+            //foreach (var item in Num) 
+            //{
+            //    Console.Write($"{item}\t");
+
+            //}
             #endregion
+
+
+            #region Q2: Create a generic Range<T> class
+
+            //Assigment.Range<int> NumRange = new Assigment.Range<int>(20, 10);
+
+            //NumRange.IsInRange(15);  // True
+            //NumRange.IsInRange(25);  // False
+           
+            //Assigment.Range<string> StringRange = new Assigment.Range<string>("Ahmed", "Yahia");
+
+            //StringRange.IsInRange("Ali");  // True
+            //StringRange.IsInRange("Ziad");   // False 
+           
+            #endregion
+
+
         }
     }
 }
